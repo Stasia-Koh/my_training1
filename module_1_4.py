abd99a6a5ccc73e69e81d37396ca5b5fd56048b5
Stasia-Koh <nastea28033@gmail.com>
2024-11-12 23:34:31 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+name = "Меня зовут Анастасия"
+print(len(name))
+print('Меня зовут Анастасия'.upper())
+print('Меня зовут Анастасия'.lower())
+print('Меня зовут Анастасия'.replace(' ', ''))
+print(name[0])
+print(name[-1])
