0e0514ef61b6c8d4cb19bc9289498f38d2a0bcd5
Stasia-Koh <nastea28033@gmail.com>
2024-11-13 05:20:26 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,14 @@
+immutable_var = 1, 2, 'a', 'b'
+print(immutable_var)
+
+mutable_list = (1, 2, 'a', 'b')
+print(mutable_list)
+
+mutable_list = ([1, 2], 'a', 'b')
+print(mutable_list)
+mutable_list[0][0] = 3
+print(mutable_list)
+
+
+
+
