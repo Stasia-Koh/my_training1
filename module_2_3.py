7db1d9db62e2e61580091368ee29b11490237984
Stasia-Koh <nastea28033@gmail.com>
2024-11-18 18:11:44 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+
+while i < len(my_list):
+    if my_list:
+        print(my_list[0])
+        print(my_list[1])
+        print(my_list[2])
+        print(my_list[3])
+        print(my_list[5])
+
+        break
