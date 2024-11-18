5e0c1823ef82d4ca1ea423eaec80133731703a8e
Stasia-Koh <nastea28033@gmail.com>
2024-11-18 18:23:38 +0500

    Add files via upload

--- module_2_3.py
+++ module_2_3.py
@@ -1,12 +1,13 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 i = 0
+elem = my_list[i]
 
-while i < len(my_list):
-    if my_list:
-        print(my_list[0])
-        print(my_list[1])
-        print(my_list[2])
-        print(my_list[3])
-        print(my_list[5])
-
+while elem>= 0 and i < len(my_list):
+    elem = my_list[i]
+    i += 1
+    if elem < 0:
         break
+    elif elem == 0:
+        continue
+    print(elem)
+
